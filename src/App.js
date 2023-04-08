24006b78c29aec831212dbed16b8de55558eea70
sundar s <santhoshvkumar@whileofone.com>
2023-04-08 09:30:32 +0530

    Portfolio page added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import earth from './earth.svg';
+// import earth from './earth.svg';
 // import logo from './logo.svg';
 import './App.css';
+import Portfolio from './pages/portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={earth} className="App-logo" alt="logo" />
-        <p>
-          Hi Welcome!
-        </p>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={earth} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hi Welcome!
+    //     </p>
+    //   </header>
+    // </div>
+    <Portfolio />
   );
 }
 
---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+
+import earth from '../earth.svg';
+import './portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className="wrapper">
+      <div className="card">
+        <img src={earth} className="App-logo" alt="logo" />
+        <span>Hi I am Sathya !</span>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
